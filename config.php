36a46a1b46f14c9ed6971ddaede139891525cf33
IlyaZholobov <ilyazh47@gmail.com>
2021-08-03 15:34:01 +0300

    Test task Machaon

---
+++ config.php
@@ -0,0 +1,61 @@
+<?php
+    /**
+     * Get config
+     * 
+     * @return array
+     */
+    function getSettings() {
+        return [
+            "site_name" => "My site",
+            "site_url" => "http://mysite.ru",
+            "assets" => [
+                "version" => 2,
+                "minify" => true,
+            ],
+            "db" => [
+                "user" => "admin",
+                "password" => "ifghigh8y8rt347ghi",
+                "name" => "my_database"
+            ],
+            "app" => [
+                "services" => [
+                    "resizer" => [
+                        "prefer_format" => "webp",
+                        "fallback_format" => "jpeg"
+                    ]
+                ]
+            ]
+        ];
+    }
+        
+    /**
+     * Get config option value 
+     * 
+     * @param string $optionName
+     * @param string $default
+     * 
+     * @return string|array
+     * @throw Exception
+     */
+    function getConfig($optionName, $default = '') {
+        $keys = explode(".", $optionName);
+        $optionValue = getSettings();
+        
+        try{
+            foreach ($keys as $key){
+            if (isset($optionValue[$key]) === true){
+                $optionValue = $optionValue[$key];
+            }elseif (empty($default) === false){
+                return $default;
+            }else{
+                throw new Exception("Option not installed");
+            }
+        };
+        
+        return  $optionValue;
+        }catch (Exception $e) {
+            echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+        }
+    }
+  
+echo (getConfig("db.hosts"));
